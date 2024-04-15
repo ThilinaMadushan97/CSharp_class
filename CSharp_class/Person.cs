5a0b4804b3f1e148ed62a02c8ce60f58ce09d8b3
Thilina Madushan <satmadushan97@gmail.com>
2024-04-15 21:26:10 +0530

    Added required for Last name

---
+++ CSharp_class/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_class
+{
+    internal class Person
+    {
+        public string? FirstName {  get; set; }
+        public required string LastName { get; init; }
+
+        public decimal Salary { get; set; }
+    }
+}
--- CSharp_class/Program.cs
+++ CSharp_class/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using CSharp_class;
+
+var person1 = new Person
+{ 
+    LastName = "Madushan" 
+};
+
+person1.FirstName = "Thilina";
+person1.Salary = 18500;
+
+
+Console.WriteLine($"{person1.FirstName} {person1.LastName}, Salary is {person1.Salary}");
